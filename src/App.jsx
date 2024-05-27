1173bdef9402ff89a92e2cfff1433e9a3d1d461b
primeboogie <primemarkboogie@gmail.com>
2024-05-27 17:06:17 -0100

    second initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '/public/cloud.jpeg'
+import Home from '@pages/Home'
+
+function App() {
+  return (
+    <>
+    <h1>Cloud Miner</h1>
+    <Home/>
+    </>
+  )
+}
+
+export default Ap
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@components/ui/Button.jsx' 
+
+function Home() {
+  return (
+<>
+<h1>CLOUD-MINNER</h1>
+    </>
+  )
+}
+
+export default Home
